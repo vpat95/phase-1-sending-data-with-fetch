0f670a4193f30bbdd3522ca830518d2eab33efc7
Vivek Patel <vpatel360@gmail.com>
2023-07-15 14:05:47 -0400

    commit tree 63377af8071c810767e7709410a14169c24b984a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: userName,
+                              email: userEmail})
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        return document.body.append(object.id)
+    })
+    .catch(function(error){
+        return document.body.append(error.message)
+    })
+}
